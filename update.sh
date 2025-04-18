c4bba1e4af132c2b9a11f87c0321d1b9bd210398
eren <eren@erendeMacBook-Air.local>
2025-04-18 22:01:37 +0900

    update

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 git add .
 git commit -m "update"
 git push origin main
-cp /Users/eren/Desktop/git_workspace/rigennka/作品/kk/KK🇯🇵.md /Volumes/Kindle/documents/Downloads/kk.txt -f
+cp /Users/eren/Desktop/git_workspace/rigennka/作品/kk/KK🇯🇵.md /Volumes/Kindle/documents/Downloads/        
